95e095f836c80f071cc5ae9fa420763101675238
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-04-02 12:26:33 +0530

    chore: first run

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import p5 from 'p5';
 
-const sketch = (p: p5) => {
-    p.setup = () => {
-        p.createCanvas(800, 600);
-    };
+function setup(p: p5) {
+    p.createCanvas(500, 500);
+    p.background(220);
 
-    p.draw = () => {
-        p.background(0);
-        p.fill(255);
-        p.rect(100, 100, 100, 100);
-    };
 }
 
-new p5(sketch);
+function draw(p: p5) {
+    // draw a circle
+    if (p.mouseIsPressed) {
+        p.fill(0);
+    }
+    else {
+        p.fill(255);
+    }
+    p.ellipse(p.mouseX, p.mouseY, 80, 80);
+}
+
+new p5((p: p5) => {
+    p.setup = () => setup(p);
+    p.draw = () => draw(p);
+});
